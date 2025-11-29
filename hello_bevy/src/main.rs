d5485aeed3cf74157169a5a408ec804caa3ac6a0
Sergey Tihon <sergey.tihon@gmail.com>
2025-11-29 20:13:32 +0100

    feat: add dragon 😆

--- hello_bevy/src/main.rs
+++ hello_bevy/src/main.rs
@@ -1,5 +1,19 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Dragon;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    let dragon_image = asset_server.load("dragon.png");
+    commands
+        .spawn(Sprite::from_image(dragon_image))
+        .insert(Dragon);
+}
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
